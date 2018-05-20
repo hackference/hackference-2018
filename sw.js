784206043125175d1e36ca6d9deb111d82f2f4c7
Mike Elsmore <mike@elsmore.me>
2018-05-20 18:05:16 +0100

    Change the way the fetch is done

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'hackference-v2';
+var CACHE_NAME = 'hackference-v3';
 var urlsToCache = [
   // Pages
   '/',
@@ -24,32 +24,49 @@ var urlsToCache = [
   '/assets/favicons/mstile-150x150.png',
   '/assets/favicons/mstile-310x150.png',
   '/assets/favicons/mstile-310x310.png',
-  '/assets/favicons/safari-pinned-tab.svg',
+  '/assets/favicons/safari-pinned-tab.svg'
 ];
 
 self.addEventListener('install', function(event) {
-  console.log('Cache - Install Step')
+  console.log('Cache - Install Step');
   // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 self.addEventListener('fetch', function(event) {
-  console.log('Cache - Fetch Event')
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        console.log('Cache - Respond From Cache')
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        //console.log("cache request: " + event.request.url);
+        var fetchPromise = fetch(event.request).then(
+          function(networkResponse) {
+            // if we got a response from the cache, update the cache
+            console.log(
+              'fetch completed: ' + event.request.url,
+              networkResponse
+            );
+            if (networkResponse) {
+              console.debug(
+                'updated cached page: ' + event.request.url,
+                networkResponse
+              );
+              cache.put(event.request, networkResponse.clone());
+            }
+            return networkResponse;
+          },
+          function(e) {
+            // rejected promise - just ignore it, we're offline
+            console.log('Error in fetch()', e);
+          }
+        );
+
+        // respond from the cache, or the network
+        return response || fetchPromise;
+      });
+    })
   );
 });
